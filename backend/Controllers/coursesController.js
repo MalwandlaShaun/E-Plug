c5f57929f968528e5e99f5a6b2aab2c513c0f299
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2023-10-10 09:10:48 +0200

    adding controller files 2

---
+++ backend/Controllers/coursesController.js
@@ -0,0 +1,7 @@
+const createCourse = () => {};
+const deleteCourse = () => {};
+const updateCourse = () => {};
+const getAllCourses = () => {};
+const getCourseById = () => {};
+const getEnrolled = () => {};
+const getFacilitator = () => {};
