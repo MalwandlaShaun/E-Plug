a5f164f6ab2a7f2ecd058bebee27422399ffbe80
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2023-10-10 09:08:46 +0200

    adding controller files

---
+++ backend/Controllers/courseContentController.js
@@ -0,0 +1,20 @@
+const createCourseContent = async (req, res, next) => {};
+
+deleteCourseContent();
+updateCourseContent();
+getAllCourseContent();
+getCourseContent();
+getSectionById();
+addSection();
+deleteSection();
+updateSection();
+getSection();
+getTopic();
+getTopicById();
+deleteTopic();
+updateTopic();
+addTopic();
+getLessonById();
+getAllLessons();
+deleteLesson();
+updateLesson();
