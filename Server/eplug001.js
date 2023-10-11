64ec5e20a338acc8cbe46982dbf8532ead085e36
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-10-11 11:46:16 +0200

    feat: add .gitIgnore file

--- Server/eplug001.js
+++ Server/eplug001.js
@@ -8,7 +8,6 @@ const mongoose = require("mongoose");
 const https = require("https");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
-const ip = "10.255.66.152"; // change this to suit context
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -18,7 +17,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/eplug");
 
 //Routes for user authentication and authorization using JWT tokens
 
-app.listen(8080, ip, function () {
+app.listen(8080, function () {
   console.log(
     "server started...\nClick the url to gain access: http://10.255.66.152:8080/"
   );
